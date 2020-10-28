62970ab5215b89fa1e4112fd14c524bc26d88f2f
Abhishek Anand <abhirk247@gmail.com>
2020-10-28 21:39:46 +0530

    array half lecture

---
+++ tut7.js
@@ -0,0 +1,28 @@
+console.log("Array in js");
+
+let marks = [13,53,34,64,75,86]
+
+let fruits = ["Apple", "Banana", "grapes","cherry"]
+let mixed = ["str", 34,43,[32,56,64]];
+let arr= new Array(64,45,"Abhishek");
+
+
+// console.log(marks)
+// console.log(marks[5])
+// console.log(fruits)
+// console.log(mixed)
+// console.log(arr[0])
+// console.log(arr.length)
+// console.log(Array.isArray("Abhishek"))
+
+// arr[0]= "Abhi";
+// console.log(arr);
+
+// let value = marks.indexOf(64)
+// marks.push(898);
+// marks.unshift(100);
+//  marks.pop()
+//  marks.shift()
+
+marks.splice()
+console.log(marks)
